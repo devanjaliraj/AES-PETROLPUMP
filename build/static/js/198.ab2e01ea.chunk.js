f17db29b420e01f61a02cc573132651547813249
devanjaliraj <anjalidevelopersveltosest@gmail.com>
2022-04-18 19:25:03 +0530

    donero-cof

--- build/static/js/198.ab2e01ea.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpvuexy-react-admin-dashboard"]=this["webpackJsonpvuexy-react-admin-dashboard"]||[]).push([[198],{1977:function(a,s){}}]);
-//# sourceMappingURL=198.ab2e01ea.chunk.js.map
--- build/static/js/200.e9542ce4.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpvuexy-react-admin-dashboard"]=this["webpackJsonpvuexy-react-admin-dashboard"]||[]).push([[200],{2012:function(a,s){}}]);
-//# sourceMappingURL=200.e9542ce4.chunk.js.map
